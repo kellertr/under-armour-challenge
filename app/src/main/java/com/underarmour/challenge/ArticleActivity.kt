e535bc40f1b31a2bd2b0f727f20978f22208aeb9
Trey Keller <trey.r.keller@lowes.com>
2019-01-26 18:07:27 -0500

    Initial Commit

---
+++ app/src/main/java/com/underarmour/challenge/ArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.underarmour.challenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
